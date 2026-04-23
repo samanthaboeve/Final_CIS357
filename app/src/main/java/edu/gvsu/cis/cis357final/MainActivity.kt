7044a10f8a8db6c3f7cf8baac70cec4e4473749a
Samantha Boeve <boevesa@mail.gvsu.edu>
2026-04-23 12:44:44 -0400

    testing pushing files

---
+++ app/src/main/java/edu/gvsu/cis/cis357final/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.gvsu.cis.cis357final
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.gvsu.cis.cis357final.ui.theme.CIS357FINALTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CIS357FINALTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "CIS 357 FINAL",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CIS357FINALTheme {
+        Greeting("Android")
+    }
+}
