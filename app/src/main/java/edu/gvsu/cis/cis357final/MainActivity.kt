c015df283800dd8f2a649359d406e536c2c55ca7
Samantha Boeve <boevesa@mail.gvsu.edu>
2026-04-23 13:06:24 -0400

    Test commit

--- app/src/main/java/edu/gvsu/cis/cis357final/MainActivity.kt
+++ app/src/main/java/edu/gvsu/cis/cis357final/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import edu.gvsu.cis.cis357final.ui.theme.CIS357FINALTheme
 
+//TEST TEST TEST
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
