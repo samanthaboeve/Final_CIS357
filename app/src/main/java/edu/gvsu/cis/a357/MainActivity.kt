ee6beb9562c24ba82a4b2fedc9582ff0da6624b1
Samantha Boeve <boevesa@mail.gvsu.edu>
2026-04-29 06:03:11 -0400

    test

--- app/src/main/java/edu/gvsu/cis/a357/MainActivity.kt
+++ app/src/main/java/edu/gvsu/cis/a357/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             _357Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "test",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
